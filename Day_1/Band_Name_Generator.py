213a8cbdae700a43e0ce54849971a6de002108cc
Skeeter-Man <Michael_Newman@Yamaha-Motor.com>
2025-06-11 20:03:34 -0500

    added comment

--- Day_1/Band_Name_Generator.py
+++ Day_1/Band_Name_Generator.py
@@ -1,3 +1,4 @@
+# Band Name Generator app
 print("Hello Friends, Welcome to the Band Name Generator")
 city = input("What city did you grow up in?\n")
 pets_name = input("what is the name of your pet?\n")
