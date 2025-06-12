e0a58f65686d54598461bbd6e99f6ab5fc4aa95e
Skeeter-Man <Michael_Newman@Yamaha-Motor.com>
2025-06-11 20:00:00 -0500

    Init

---
+++ Day_1/Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Hello Friends, Welcome to the Band Name Generator")
+city = input("What city did you grow up in?\n")
+pets_name = input("what is the name of your pet?\n")
+print(f"You band name could be {city} {pets_name}")
